49f7f079c0852e34f5d5a08cd04544d0051ba83e
Dmytro Lynda <dmytrolynda@gmail.com>
2025-09-08 12:40:42 +0200

    Add violation at the file start

--- routes/login.ts
+++ routes/login.ts
@@ -1,18 +1,15 @@
-/*
- * Copyright (c) 2014-2024 Bjoern Kimminich & the OWASP Juice Shop contributors.
- * SPDX-License-Identifier: MIT
- */
-
 import models = require('../models/index')
-import { type Request, type Response, type NextFunction } from 'express'
-import { type User } from '../data/types'
-import { BasketModel } from '../models/basket'
-import { UserModel } from '../models/user'
-import challengeUtils = require('../lib/challengeUtils')
-import config from 'config'
-import { challenges } from '../data/datacache'
+models.sequelize.query(`SELECT * FROM Users WHERE email = '${req.body.email || ''}' AND password = '${security.hash(req.body.password || '')}' AND deletedAt IS NULL`, { model: UserModel, plain: true }) // vuln-code-snippet vuln-line loginAdminChallenge loginBenderChallenge loginJimChallenge
 
-import * as utils from '../lib/utils'
+//import { type Request, type Response, type NextFunction } from 'express'
+//import { type User } from '../data/types'
+//import { BasketModel } from '../models/basket'
+//import { UserModel } from '../models/user'
+//import challengeUtils = require('../lib/challengeUtils')
+//import config from 'config'
+//import { challenges } from '../data/datacache'
+
+//import * as utils from '../lib/utils'
 const security = require('../lib/insecurity')
 const users = require('../data/datacache').users
 
