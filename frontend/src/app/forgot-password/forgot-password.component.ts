bfb724d85cb819cf112bdff0acca5be297552d89
Dmytro Lynda <dmytro@cycode.com>
2026-04-13 12:23:28 +0200

    Custom branch commit

--- frontend/src/app/forgot-password/forgot-password.component.ts
+++ frontend/src/app/forgot-password/forgot-password.component.ts
@@ -26,9 +26,12 @@ export class ForgotPasswordComponent {
   public passwordControl: UntypedFormControl = new UntypedFormControl({ disabled: true, value: '' }, [Validators.required, Validators.minLength(5)])
   public repeatPasswordControl: UntypedFormControl = new UntypedFormControl({ disabled: true, value: '' }, [Validators.required, matchValidator(this.passwordControl)])
   public securityQuestion?: string
-  public error?: string
+
 
   
+  public error?: string
+
+
   public confirmation?: string
 
 
